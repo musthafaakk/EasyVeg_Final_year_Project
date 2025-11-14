e81ea163cd85e7eb6680a98387473f46a865be5b
musthafa <musthafaak728@gmail.com>
2025-11-14 14:58:13 +0530

    push2

--- Easyveg/Slice2Cook/models.py
+++ Easyveg/Slice2Cook/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class user_registration(models.Model):
+    name=models.CharField(max_length=30)
+    email=models.EmailField(max_length=60)
+    password=models.CharField(max_length=10)
+    state=models.CharField(max_length=20)
+    district=models.CharField(max_length=20)
+    address=models.CharField(max_length=50)
+    pincode=models.CharField(max_length=6)
+    phonenumber=models.CharField(max_length=10)
+
+
+
+
+
+
--- Easyveg/Slice2Cook/views.py
+++ Easyveg/Slice2Cook/views.py
@@ -1,11 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . models import *
+from django.contrib import messages
 # Create your views here.
 def index(request):
     return render(request,"index.html")
 def userlog(request):
     return render(request,"user/userlog.html")
 def userreg(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        confirmpassword=request.POST.get("confirmpassword")
+        state=request.POST.get("state")
+        district=request.POST.get("district")
+        address=request.POST.get("address")
+        pincode=request.POST.get("pincode")
+        phonenumber=request.POST.get("phonenumber")
+        if password==confirmpassword:
+            if user_registration.objects.filter(email=email).exists():
+                messages.info(request,'This email is alresdy in use')
+            elif user_registration.objects.filter(phonenumber=phonenumber).exists():
+                messages.info(request,'This number is already in use')
+            else :
+                userdata=user_registration(name=name,email=email,password=password,state=state,district=district,address=address,pincode=pincode,phonenumber=phonenumber)
+                userdata.save()
+                return redirect("userlog")
+        else:
+            messages.info(request,'password not matched')
+
+
     return render(request,"user/userreg.html")
 def userhome(request):
     return render(request,"user/userhome.html")
